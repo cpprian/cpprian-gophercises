2a4ef598c92ee480a53d9bdb2fb908e8bf760279
cpprian <cpprian456@gmail.com>
2024-04-13 14:10:06 +0200

    add concurrency to quiz-game

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 type QuestionAnswer struct {
@@ -21,9 +22,11 @@ type QuizGame struct {
 }
 
 func main() {
+	done := make(chan bool)
+
 	// load user's flags
 	var filenameFlag = flag.String("f", "problem.csv", "provide a csv file from which you want to learn")
-
+	var timeFlag = flag.Int("t", 10, "set time for your session in seconds")
 	flag.Parse()
 
 	// load a csv file
@@ -40,7 +43,16 @@ func main() {
 		return
 	}
 
-	quizgame.startGame()
+	go func() {
+		quizgame.startGame()
+		done <- true
+	}()
+
+	select {
+	case <-done:
+	case <-time.After(time.Duration(*timeFlag) * time.Second):
+		fmt.Println("\ntimeout!")
+	}
 
 	fmt.Printf("Your score: %d/%d\n", quizgame.correctAnswer, len(quizgame.qa))
 }
